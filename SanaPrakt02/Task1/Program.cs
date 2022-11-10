b63d32146ab182ce2392e09b1fd76a8bccc005bd
Andriy Lisoviy <sana22_layu@student.ztu.edu.ua>
2022-11-10 12:16:14 +0200

    change3lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Console Project v1");
+Console.WriteLine("Console Project v2");
+Console.WriteLine("Console Project v3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 
