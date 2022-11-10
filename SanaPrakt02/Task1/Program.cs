1684ca2220f668f526625fb5e84837f05111bcfa
Andriy Lisoviy <sana22_layu@student.ztu.edu.ua>
2022-11-10 12:17:22 +0200

    change2lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Console Project v1");
 Console.WriteLine("Console Project v2");
 Console.WriteLine("Console Project v3");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Project v1");
+Console.WriteLine("Project v2");
 
